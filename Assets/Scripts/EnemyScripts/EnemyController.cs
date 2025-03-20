a93a9e50713ef6c23ae42bead29f2e57cc6f018f
melih can aşık <melihcanask@gmail.com>
2025-03-20 15:31:32 +0300

    Obstacle based Pathfinding for Enemies

--- Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -9,6 +9,8 @@ namespace EnemyScripts
         public float speed = 3f;
         public float rotateSpeed = 0.0025f;
         private Rigidbody2D rb;
+        public float obstacleAvoidanceDistance = 1.5f;
+        public LayerMask obstacleLayer;
 
         private void Start()
         {
@@ -25,12 +27,18 @@ namespace EnemyScripts
             {
                 RotateTowardsTarget();
             }
-
         }
     
         private void FixedUpdate()
         {
-        rb.linearVelocity = transform.up * speed;
+            Vector2 moveDirection = transform.up;
+            
+            if (ObstacleAhead())
+            {
+                moveDirection = AvoidObstacle();
+            }
+            
+            rb.linearVelocity = moveDirection * speed;
         }
 
         private void RotateTowardsTarget()
@@ -40,6 +48,7 @@ namespace EnemyScripts
             Quaternion q = Quaternion.Euler(new Vector3(0, 0, angle));
             transform.localRotation = Quaternion.Slerp(transform.localRotation, q, rotateSpeed);
         }
+
         private void GetTarget()
         {
             if (GameObject.FindGameObjectWithTag("Player"))
@@ -48,14 +57,45 @@ namespace EnemyScripts
             }
         }
 
+        private bool ObstacleAhead()
+        {
+            return Physics2D.Raycast(transform.position, transform.up, obstacleAvoidanceDistance, obstacleLayer);
+        }
+
+        private Vector2 AvoidObstacle()
+        {
+            Vector2 leftDirection = Quaternion.Euler(0, 0, 30) * transform.up;
+            Vector2 rightDirection = Quaternion.Euler(0, 0, -30) * transform.up;
+
+            bool leftClear = !Physics2D.Raycast(transform.position, leftDirection, obstacleAvoidanceDistance, obstacleLayer);
+            bool rightClear = !Physics2D.Raycast(transform.position, rightDirection, obstacleAvoidanceDistance, obstacleLayer);
+
+            if (leftClear && rightClear)
+            {
+                return UnityEngine.Random.value > 0.5f ? leftDirection : rightDirection;
+            }
+            else if (leftClear)
+            {
+                return leftDirection;
+            }
+            else if (rightClear)
+            {
+                return rightDirection;
+            }
+            else
+            {
+                return -transform.up; // Reverse direction if no clear path
+            }
+        }
+
         private void OnCollisionEnter2D(Collision2D other)
         {
-         
-            if(other.gameObject.CompareTag("Player"))
+            if (other.gameObject.CompareTag("Player"))
             {
                 Destroy(other.gameObject);
                 target = null;
-            } else if(other.gameObject.CompareTag("Bullet"))
+            }
+            else if (other.gameObject.CompareTag("Bullet"))
             {
                 Destroy(other.gameObject);
                 Destroy(gameObject);
--- Assets/Scripts/EnemyScripts/RangedEnemy.cs
+++ Assets/Scripts/EnemyScripts/RangedEnemy.cs
@@ -3,24 +3,27 @@ using UnityEngine;
 
 namespace EnemyScripts
 {
-    public class RangedEnemy : MonoBehaviour
+  
+
+public class RangedEnemy : MonoBehaviour
     {
         public Transform target;
         public float speed = 3f;
         public float rotateSpeed = 0.0025f;
         private Rigidbody2D rb;
         public GameObject bulletPrefab;
-
         public float distanceToShoot = 5f;
         public float distanceToStop = 3f;
-        
         public Transform firingPoint;
         public float fireRate;
         private float timeToFire;
+        public float obstacleAvoidanceDistance = 1.5f;
+        public LayerMask obstacleLayer;
+
         private void Start()
         {
             rb = GetComponent<Rigidbody2D>();
-            timeToFire= 0f;
+            timeToFire = 0f;
         }
 
         private void Update()
@@ -30,7 +33,7 @@ namespace EnemyScripts
                 GetTarget();
             }
             else
-            { 
+            {
                 RotateTowardsTarget();
             }
 
@@ -40,6 +43,75 @@ namespace EnemyScripts
             }
         }
 
+        private void FixedUpdate()
+        {
+            if (target != null)
+            {
+                Vector2 moveDirection = transform.up;
+
+                if (Vector2.Distance(target.position, transform.position) >= distanceToStop)
+                {
+                    if (ObstacleAhead())
+                    {
+                        moveDirection = AvoidObstacle();
+                    }
+                    rb.linearVelocity = moveDirection * speed;
+                }
+                else
+                {
+                    rb.linearVelocity = Vector2.zero;
+                }
+            }
+        }
+
+        private void RotateTowardsTarget()
+        {
+            Vector2 targetDirection = target.position - transform.position;
+            float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
+            Quaternion q = Quaternion.Euler(new Vector3(0, 0, angle));
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, q, rotateSpeed);
+        }
+
+        private void GetTarget()
+        {
+            if (GameObject.FindGameObjectWithTag("Player"))
+            {
+                target = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+        }
+
+        private bool ObstacleAhead()
+        {
+            return Physics2D.Raycast(transform.position, transform.up, obstacleAvoidanceDistance, obstacleLayer);
+        }
+
+        private Vector2 AvoidObstacle()
+        {
+            Vector2 leftDirection = Quaternion.Euler(0, 0, 30) * transform.up;
+            Vector2 rightDirection = Quaternion.Euler(0, 0, -30) * transform.up;
+
+            bool leftClear = !Physics2D.Raycast(transform.position, leftDirection, obstacleAvoidanceDistance, obstacleLayer);
+            bool rightClear = !Physics2D.Raycast(transform.position, rightDirection, obstacleAvoidanceDistance, obstacleLayer);
+
+            if (leftClear && rightClear)
+            {
+                return UnityEngine.Random.value > 0.5f ? leftDirection : rightDirection;
+            }
+            else if (leftClear)
+            {
+                return leftDirection;
+            }
+            else if (rightClear)
+            {
+                return rightDirection;
+            }
+            else
+            {
+                return -transform.up; // Reverse direction if no clear path
+            }
+            
+            
+        }
         private void Shoot()
         {
             if(timeToFire <= 0)
@@ -50,50 +122,6 @@ namespace EnemyScripts
             else
             {
                 timeToFire -= Time.deltaTime;
-            }
-        }
-        private void FixedUpdate()
-        {
-            if (target != null)
-            {
-                if (Vector2.Distance(target.position, transform.position) >= distanceToStop)
-                {
-                    rb.linearVelocity = transform.up * speed;
-                }
-                else
-                {
-                    rb.linearVelocity = Vector2.zero;
-                }
-            }
-           
-        }
-
-        private void RotateTowardsTarget()
-        {
-            Vector2 targetDirection = target.position - transform.position;
-            float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
-            Quaternion q = Quaternion.Euler(new Vector3(0, 0, angle));
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, q, rotateSpeed);
-        }
-        private void GetTarget()
-        {
-            if (GameObject.FindGameObjectWithTag("Player"))
-            {
-                target = GameObject.FindGameObjectWithTag("Player").transform;
-            }
-        }
-
-        private void OnCollisionEnter2D(Collision2D other)
-        {
-         
-            if(other.gameObject.CompareTag("Player"))
-            {
-                Destroy(other.gameObject);
-                target = null;
-            } else if(other.gameObject.CompareTag("Bullet"))
-            {
-                Destroy(other.gameObject);
-                Destroy(gameObject);
             }
         }
     }
